4de815e206f6319bb083971c8c73d9a97ad86a9b
Gorkem97 <gorkemucar3@hotmail.com>
2022-04-16 02:43:47 +0300

    dönme olayı eklendi

--- Assets/GorkemScripts/MovementScript.cs
+++ Assets/GorkemScripts/MovementScript.cs
@@ -7,6 +7,7 @@ public class MovementScript : MonoBehaviour
     public CharacterController controller;
 
     public float speed = 6f ;
+    public float smoothness = 60f;
     void Start()
     {
         
@@ -18,10 +19,34 @@ public class MovementScript : MonoBehaviour
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
+        Vector3 mouseplace = Camera.main.ScreenToWorldPoint(Input.mousePosition)-transform.position;
+        Quaternion target = Quaternion.Euler(horizontal, 0, vertical);
 
         if (direction.magnitude > 0)
         {
             controller.Move(direction * speed * Time.deltaTime);
         }
+
+        
+        
+
+            //Get the Screen positions of the object
+            Vector3 positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
+
+            //Get the Screen position of the mouse
+            Vector3 mouseOnScreen = (Vector3)Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+            //Get the angle between the points
+            float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
+
+            //Ta Daaa
+            transform.rotation = Quaternion.Euler(new Vector3(0f, -angle, 0f));
     }
+
+    float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
+    {
+            return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+    }
+
+    
 }
