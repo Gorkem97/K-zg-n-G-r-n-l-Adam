bbfe986edbae34da55961811abfb0ba0e28d4118
Ege <79145337+EgedotErcan@users.noreply.github.com>
2022-04-17 10:20:45 +0300

    merged : map and movement script
    Co-Authored-By: Gorkem97 <76035137+Gorkem97@users.noreply.github.com>

---
+++ Assets/EgeScript/Destructible.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructible : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject destroyedVersion;
+    [SerializeField]
+    private float break_force;
+
+    PickUpSystem pick_up_system;
+
+    private void Awake()
+    {
+
+        pick_up_system = GameObject.FindGameObjectWithTag("Player").GetComponent<PickUpSystem>();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("ground") && pick_up_system.can_destruct)
+        {
+            Break_thing();
+            pick_up_system.can_destruct = false;
+        }
+    }
+    private void Break_thing()
+    {
+        GameObject broken_object = Instantiate(destroyedVersion, transform.position, transform.rotation);
+
+        foreach (Rigidbody rb in broken_object.GetComponentsInChildren<Rigidbody>())
+        {
+            Vector3 force = (rb.transform.position - transform.position).normalized * break_force;
+            rb.AddForce(force);
+        }
+        Destroy(gameObject);
+    }
+}
